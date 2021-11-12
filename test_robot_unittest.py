572eef22c4c5a5cfdae451a65f1b57b5ce2de26d
Reza Hose <reza.hoseiny@sydney.edu>
2021-11-12 19:37:04 +1100

    adding table class and define some methods to return/set x and y dimensions, (table.py)

--- test_robot_unittest.py
+++ test_robot_unittest.py
@@ -14,7 +14,7 @@ class TestRobotMethods(unittest.TestCase):  #every test class must inherit from 
         self.robot = Robot(self.robot_name)
         self.x_initial, self.y_initial = 0,0
         self.face_initial = Face.EAST
-        # fabrik a typical table of dimensions 5 units x 5 units and pass it to the robot
+        # fabricate a table of dimensions 5 units x 5 units and pass it to the robot
         x_dimension = 5
         y_dimension = 5
         self.test_table_one = Table("table one", x_dimension, y_dimension)
@@ -32,11 +32,11 @@ class TestRobotMethods(unittest.TestCase):  #every test class must inherit from 
         self.assertEqual(self.robot.getCurrentFaceDirection(), self.face_initial)
 
     def test_set_table(self):
-        
         self.assertEqual(self.robot.getTable().getTableName(), "table one")
 
     def test_get_table_dimensions(self):
         (x,y) = self.robot.getTableDimensions()
+        self.assertEqual((x,y), (x_dimension, y_dimension))
 
 
 
